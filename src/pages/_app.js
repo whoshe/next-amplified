0447ee3c6ac4ca0b40a698dbe3638dd48af2ce33
Changwoo Nam <whoshe@funnyplan.com>
2023-08-13 15:17:00 +0900

    amplified test code updated4.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,7 @@
 import "../styles/globals.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "@/aws-exports";
+Amplify.configure({ ...awsExports, ssr: true });
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
